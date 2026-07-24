a2c05777f53e1af72834340e7851c7290b60a39a
Juanplago <juanchinpg@gmail.com>
2026-07-24 11:19:40 -0500

    VFX
    Water and hit vfx

--- Assets/CorgiRebrushed/Characters/Corgi/Abilities/WaterJump.cs
+++ Assets/CorgiRebrushed/Characters/Corgi/Abilities/WaterJump.cs
@@ -1,11 +1,22 @@
 using System;
+using System.Collections;
+using PrimeTween;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class WaterJump : Abilitiy
 {
     [SerializeField] [FoldoutGroup("References")] private Rigidbody _rigidbody;
-    
+    [SerializeField] [FoldoutGroup("References")] private ForcesBasedCharacterMovementController _movementController;
+    [SerializeField] [FoldoutGroup("References")] private VisualEffect _waterCascadeVfxPrefab;
+
+    [SerializeField] [FoldoutGroup("Data")] private float _vfxGroundOffset = -0.95f;
+    [SerializeField] [FoldoutGroup("Data")] private float _vfxActiveDuration = 3f;
+    [SerializeField] [FoldoutGroup("Data")] private float _vfxGrowInDuration = 0.5f;
+    [SerializeField] [FoldoutGroup("Data")] private float _vfxFadeOutBuffer = 1f;
+    [SerializeField] [FoldoutGroup("Data")] private float _boostSuppressionDuration = 0.5f;
+
     // Cache an array for non-allocating physics checks (Max 20 targets per hit)
     private readonly Collider[] hitBuffer = new Collider[20];
 
@@ -16,25 +27,64 @@ public class WaterJump : Abilitiy
 
     private void OnDisable()
     {
-        _abilityEventAsset.OnInvoked.AddListener(WaterImpulse);
+        _abilityEventAsset.OnInvoked.RemoveListener(WaterImpulse);
     }
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _movementController = GetComponent<ForcesBasedCharacterMovementController>();
     }
     
     private void WaterImpulse(ECorgiHability context)
     {
         if (context != ECorgiHability.Water) return;
-        
+
         ApplyImpulseForce();
+        PlayWaterJetVfx();
         ApplyDamage();
     }
 
     private void ApplyImpulseForce()
     {
         _rigidbody.AddForce(_characterObject.up * _abilitiesData.WaterJumpImpulseForce, ForceMode.Impulse);
+
+        if (_movementController == null) return;
+
+        _movementController.ExternalBoostActive = true;
+        CancelInvoke(nameof(ClearBoostFlag));
+        Invoke(nameof(ClearBoostFlag), _boostSuppressionDuration);
+    }
+
+    private void ClearBoostFlag()
+    {
+        _movementController.ExternalBoostActive = false;
+    }
+
+    private void PlayWaterJetVfx()
+    {
+        if (_waterCascadeVfxPrefab == null) return;
+
+        Vector3 spawnPosition = _characterObject.position + Vector3.up * _vfxGroundOffset;
+        VisualEffect vfxInstance = Instantiate(_waterCascadeVfxPrefab, spawnPosition, Quaternion.identity);
+
+        Transform vfxTransform = vfxInstance.transform;
+        Vector3 targetScale = vfxTransform.localScale;
+        vfxTransform.localScale = Vector3.zero;
+        Tween.Scale(vfxTransform, endValue: targetScale, duration: _vfxGrowInDuration);
+
+        vfxInstance.Play();
+        StartCoroutine(StopAndDestroyVfx(vfxInstance));
+    }
+
+    private IEnumerator StopAndDestroyVfx(VisualEffect vfxInstance)
+    {
+        yield return new WaitForSeconds(_vfxActiveDuration);
+        vfxInstance.Stop();
+
+        Transform vfxTransform = vfxInstance.transform;
+        Tween.Scale(vfxTransform, endValue: Vector3.zero, duration: _vfxFadeOutBuffer)
+            .OnComplete(() => Destroy(vfxTransform.gameObject));
     }
 
     private void ApplyDamage()
