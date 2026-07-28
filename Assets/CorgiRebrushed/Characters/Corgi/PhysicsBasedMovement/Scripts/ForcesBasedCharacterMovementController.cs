49235bd6110c3be56083fcb40d5edeb0eed7017f
VannessaMartinez <vmartinez2049@gmail.com>
2026-07-28 01:07:25 -0500

    chore: restore script from develop to resolve conflict

--- Assets/CorgiRebrushed/Characters/Corgi/PhysicsBasedMovement/Scripts/ForcesBasedCharacterMovementController.cs
+++ Assets/CorgiRebrushed/Characters/Corgi/PhysicsBasedMovement/Scripts/ForcesBasedCharacterMovementController.cs
@@ -1,26 +1,19 @@
 using System;
+using System.Collections;
+using PrimeTween;
 using Sirenix.OdinInspector;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
-//Audio
-using UnityEngine.Events;
 
 [RequireComponent(typeof(CharacterController))]
 [RequireComponent(typeof(Rigidbody))]
 public class ForcesBasedCharacterMovementController : CharacterController
 {
     [SerializeField][FoldoutGroup("References")] private Rigidbody _rigidbody;
-
-    //AUDIO - 
-    [FoldoutGroup("Audio")] public UnityEvent OnJump;
-    [FoldoutGroup("Audio")] public UnityEvent OnLand;
-    [FoldoutGroup("Audio")] public UnityEvent<float> OnFootstep;
-
-    private bool _wasGrounded;
-    private float _footstepTimer;
-    private bool _wasMoving;
-    //
+    
+    [ShowInInspector, FoldoutGroup("Testing")] public float CurrentAcceleration { get; set; }
+    [ShowInInspector, FoldoutGroup("Testing")] public float CurrentMaxSpeed { get; set; }
     
     // Set briefly by external abilities (e.g. WaterJump) so their impulse isn't immediately
     // cut short by the low-jump-multiplier logic below, which only expects the Jump button.
@@ -30,6 +23,9 @@ public class ForcesBasedCharacterMovementController : CharacterController
     {
         base.Awake();
         _rigidbody = GetComponent<Rigidbody>();
+        
+        CurrentAcceleration = CharacterData.Acceleration;
+        CurrentMaxSpeed = CharacterData.MaxSpeed;
     }
 
     private void Start()
@@ -39,32 +35,6 @@ public class ForcesBasedCharacterMovementController : CharacterController
 
     private void FixedUpdate()
     {
-       //AUDIO - Land detection
-       if (!_wasGrounded && IsGrounded)
-            OnLand?.Invoke();
-        _wasGrounded = IsGrounded;
-
-        //AUDIO - Footsteps audio
-        bool isMoving = _movementDirection.magnitude > 0.1f;
-        if (IsGrounded && isMoving)
-        {
-            float interval = Mathf.Lerp(0.3f, 0.59f, _movementDirection.magnitude);
-            // If the player just started moving, force the first footstep immediately
-            if (!_wasMoving)
-            {
-                _footstepTimer = interval;
-            }
-
-            _footstepTimer += Time.fixedDeltaTime;
-            if (_footstepTimer >= interval)
-            {
-                _footstepTimer = 0f;
-                OnFootstep?.Invoke(_movementDirection.magnitude);
-            }
-        }
-        _wasMoving = isMoving;
-        //AUDIO - End footsteps audio
-
         CustomFalling();
         ApplyForceToHorizontalMovement();
         CapVelocity();
@@ -75,20 +45,20 @@ public class ForcesBasedCharacterMovementController : CharacterController
         if (_movementDirection == Vector3.zero)
         {
             Vector3 horizontalVelocity = new Vector3(_rigidbody.linearVelocity.x, 0f, _rigidbody.linearVelocity.z);
-            _rigidbody.AddForce(horizontalVelocity * -_characterData.Desacceleration, ForceMode.Force);
+            _rigidbody.AddForce(horizontalVelocity * -CharacterData.Desacceleration, ForceMode.Force);
             return;
         }
         
-        _rigidbody.AddForce(_movementDirection * _characterData.Acceleration, ForceMode.Force);
+        _rigidbody.AddForce(_movementDirection * CurrentAcceleration, ForceMode.Force);
     }
     
     private void CapVelocity()
     {
         Vector3 horizontalVelocity = new Vector3(_rigidbody.linearVelocity.x, 0f, _rigidbody.linearVelocity.z);
 
-        if (horizontalVelocity.magnitude > _characterData.MaxSpeed)
+        if (horizontalVelocity.magnitude > CurrentMaxSpeed)
         {
-            Vector3 cappedVelocity = horizontalVelocity.normalized * _characterData.MaxSpeed;
+            Vector3 cappedVelocity = horizontalVelocity.normalized * CurrentMaxSpeed;
             _rigidbody.linearVelocity = new Vector3(cappedVelocity.x, _rigidbody.linearVelocity.y, cappedVelocity.z);
         }
     }
@@ -97,10 +67,10 @@ public class ForcesBasedCharacterMovementController : CharacterController
     {
         if(!IsGrounded) return;
         
-        _rigidbody.AddForce(_characterObject.up * _characterData.JumpForce, ForceMode.Impulse);
+        _rigidbody.AddForce(_characterObject.up * CharacterData.JumpForce, ForceMode.Impulse);
         
-        //TODO AUDIO - Implementar sonido de salto
-        OnJump?.Invoke();
+        //TODO Implementar sonido de salto
+        //OnJump?.Invoke();
 
     }
 
@@ -110,14 +80,13 @@ public class ForcesBasedCharacterMovementController : CharacterController
         
         if (_rigidbody.linearVelocity.y < 0)
         {
-            _rigidbody.AddForce(_characterObject.up * (Physics.gravity.y * (_characterData.FallMultiplier - 1)), ForceMode.Force);
+            _rigidbody.AddForce(_characterObject.up * (Physics.gravity.y * (CharacterData.FallMultiplier - 1)), ForceMode.Force);
             return;
         }
         
         if (_rigidbody.linearVelocity.y > 0 && !JumpAction.IsPressed() && !ExternalBoostActive)
         {
-            _rigidbody.AddForce(_characterObject.up * (Physics.gravity.y * (_characterData.LowJumpMultiplier - 1)), ForceMode.Force);
+            _rigidbody.AddForce(_characterObject.up * (Physics.gravity.y * (CharacterData.LowJumpMultiplier - 1)), ForceMode.Force);
         }
     }
-    
 }
